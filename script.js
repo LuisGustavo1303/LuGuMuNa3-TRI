78eb783999789f9f276f6160c2ac3701f2d8a186
LuisGustavo1303 <128145954+LuisGustavo1303@users.noreply.github.com>
2024-10-15 08:55:52 -0300

    .

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resulatdo");
+const caixaResultado = document.querySelector(".caixa-resulatado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?", 
         alternativas: [
-            "Isso é assustaodr!",
+            "Isso é assustador!",
             "Isso é maravilhoso!"
     ]
 },
