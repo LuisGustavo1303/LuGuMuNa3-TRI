d460c78434d5ddf309f1853763d4032a0621ac9a
LuisGustavo1303 <128145954+LuisGustavo1303@users.noreply.github.com>
2024-10-15 08:47:07 -0300

    .

--- script.js
+++ script.js
@@ -45,7 +45,7 @@ function mostraPergunta(){
     mostraAlternativas();
 }
 
-function mostraPergunta(){
+function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa;
